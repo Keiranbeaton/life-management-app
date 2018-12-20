cb87b517b4a02cd3acb900ea5394ce07ed4283f0
Keiran Beaton <Keiranbeaton@gmail.com>
2018-12-20 09:41:50 -0800

    scaffolding added for frontend

---
+++ app/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('!!file-loader?name=[name].[ext]!./html/index.html');
+require('./scss/base.scss');
+
+const angular = require('angular');
+const angularRoute = require('angularRoute');
+const lifeApp = angular.module('lifeApp', [angularRoute]);
+
+lifeApp.run(['$rootScope', ($rs) => {
+  $rs.baseUrl = `${__API_URL__}/api`;
+  $rs.httpConfig = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  };
+}]);
+
+require('./controllers')(lifeApp);
+require('./components')(lifeApp);
+// require('./services')(lifeApp);
+
+lifeApp.config(['$routeProvider', '$locationProvider', ($rp, $lp) => {
+  $lp.hashPrefix('');
+  $rp
+    .when('/home', {
+      template: require('./html/home.html')
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+}]);
