d17dfe1146148806003b17f0829434e32eb1dd71
Keiran Beaton <Keiranbeaton@gmail.com>
2018-12-25 13:20:01 -0800

    started on testing files

---
+++ backend/test/test-harness.js
@@ -0,0 +1,11 @@
+'use strict';
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+mongoose.Promise = Promise;
+process.env.APP_SECRET = 'testKey';
+require('./test-server');
+require('./auth-test');
+require('./user-test');
+process.on('exit', (code) => {
+  mongoose.connection.db.dropDatabase(() => console.log('db dropped ' + code));
+});
